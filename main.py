1453a43b15ff21ad98e21d4da9cabe0a577720b9
Rose99Oliver <rose.99.oliver@gmail.com>
2017-07-13 11:40:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import jinja2
+import os
+
+from google.appengine.ext import ndb
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class Question (ndb.Model):
+    questionText = ndb.StringProperty()
+    
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('question_input.html')
+        self.response.write(template.render())
+    def post(self):
+        question_from_form = self.request.get('question')
+        
+        new_question = Question(questionText = question_from_form)
+        question_key = new_question.put()
+
+        template = jinja_environment.get_template('question_confirm.html')
+        self.response.write(template.render(
+        {
+            'questionText': question_from_form,
+            'question_id': question_key.id()
+        }))
+
 class ListHandler(webapp2.RequestHandler):
     def get(self):
         questions_query = Question.query().order(Question.questionText)
         list_of_questions = questions_query.fetch()
         
-        template = jinja_environment.get_template('test_output.html')
+        template = jinja_environment.get_template('question_output.html')
         self.response.write(template.render(
         {
                 'list': list_of_questions,
@@ -14,7 +66,7 @@ class SingleHandler(webapp2.RequestHandler):
         question_id = self.request.get('id')
         question_id = int(question_id)
         single_question = Question.get_by_id(question_id)
-        template = jinja_environment.get_template('single_test.html')
+        template = jinja_environment.get_template('question_test.html')
         self.response.write(template.render(
             {
                 'question': single_question,
